0051ece6c3bf254c9c67f3b3560a38ac87964a50
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-07 12:41:08 +0900

    Title: 두 개의 격자 비교하기, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240907/두 개의 격자 비교하기/compare-two-grid.py
+++ 240907/두 개의 격자 비교하기/compare-two-grid.py
@@ -4,14 +4,24 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 
 array1 = []
-for i in range(4):
+for i in range(n):
     array1.append(list(map(int, input().split())))
 
 array2 = []
-for j in range(4, 8):
+for j in range(n):
     array2.append(list(map(int, input().split())))
 
 array3 = [[0] * n for _ in range(m)]
+for i in range(n):
+    for j in range(m):
+        if array1[i][j] == array2[i][j]:
+            array3[i][j] = 0
+        else:
+            array3[i][j] = 1
+
+for num in array3:
+    print(*num)
+
 for i in range(n):
     for j in range(m):
         if array1[i][j] == array2[i][j]:
