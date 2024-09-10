61e3255536776df86e605b3d6f473b5b5a848353
AnChanUng <104750924+AnChanUng@users.noreply.github.com>
2024-09-10 19:06:25 +0900

    Title: Date to Date, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240910/Date to Date/date-to-date.py
+++ 240910/Date to Date/date-to-date.py
@@ -2,6 +2,6 @@ m1, d1, m2, d2 = map(int, input().split())
 
 date = [31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365]
 
-result = (date[m2-1] + d2) - (date[m1-1] + d1)
+result = (date[m2-1] + d2) - (date[m1-1] + d1) + 1
 
 print(result)
