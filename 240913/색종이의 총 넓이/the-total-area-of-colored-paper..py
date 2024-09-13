275c3f18a3fab2fa3ecf0a7acf2db81c5e154ace
AnChanUng <104750924+AnChanUng@users.noreply.github.com>
2024-09-13 20:18:21 +0900

    Title: 색종이의 총 넓이, Time: 161ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240913/색종이의 총 넓이/the-total-area-of-colored-paper..py
@@ -0,0 +1,17 @@
+n = int(input())
+
+graph = [[0] * 101 for _ in range(101)]
+for _ in range(n):
+    x1, y1 = map(int, input().split())
+
+    for x in range(x1, x1+8):
+        for y in range(y1, y1+8):
+            graph[x][y] = 1
+            
+area = 0
+for i in range(101):
+    for j in range(101):
+        if graph[i][j] == 1:
+            area += 1
+
+print(area)
