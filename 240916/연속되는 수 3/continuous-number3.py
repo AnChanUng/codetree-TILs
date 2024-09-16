efbe2e105180668fa4db85dc2febd8462a9bc679
AnChanUng <104750924+AnChanUng@users.noreply.github.com>
2024-09-16 20:12:54 +0900

    Title: 연속되는 수 3, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240916/연속되는 수 3/continuous-number3.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+array = []
+for _ in range(n):
+    number = int(input())
+    array.append(number)
+
+cnt = 1
+max_cnt = 0
+for i in range(n):
+    if i >= 1 and array[i] > 0 and array[i-1] > 0:
+        cnt += 1
+    elif i >= 1 and array[i] < 0 and array[i-1] < 0:
+        cnt += 1
+    else:
+        cnt = 1
+
+    max_cnt = max(max_cnt, cnt)
+
+print(max_cnt)
