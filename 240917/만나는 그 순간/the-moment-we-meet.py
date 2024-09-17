228503e6409349a33d1260de8118dbe80d88f342
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-17 22:48:24 +0900

    Title: 만나는 그 순간, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240917/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,28 @@
+n, m = map(int, input().split())
+
+distance1 = 0
+array1 = []
+for _ in range(n):
+    d, t = input().split()
+    t = int(t)
+
+    if d == 'L':
+        distance1 -= t
+    elif d == 'R':
+        distance1 += t
+    
+    array1.append(distance1)
+
+distance2 = 0
+array2 = []
+for _ in range(m):
+    d, t = input().split()
+    t = int(t)
+    print(d, t)
+
+    if d == 'L':
+        distance2 -= t
+    elif d == 'R':
+        distance2 += t
+
+    array2.append(distance2)
