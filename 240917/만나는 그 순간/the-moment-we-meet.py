8b136954f7ce9c4f4d1587ff7ba966923dbc7034
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-17 22:57:25 +0900

    Title: 만나는 그 순간, Time: 210ms, Memory: 40MB, Status: Passed - CodeTree

--- 240917/만나는 그 순간/the-moment-we-meet.py
+++ 240917/만나는 그 순간/the-moment-we-meet.py
@@ -3,14 +3,14 @@ pos_a, pos_b = [0] * (1000000 + 1), [0] * (1000000 + 1)
 
 time_a = 1
 for _ in range(n):
-    d, t = tuple(input().split())
+    d, t = input().split()
     for _ in range(int(t)):
         pos_a[time_a] = pos_a[time_a - 1] + (1 if d == 'R' else -1)
         time_a += 1
 
 time_b = 1
 for _ in range(m):
-    d, t = tuple(input().split())
+    d, t = input().split()
     for _ in range(int(t)):
         pos_b[time_b] = pos_b[time_b - 1] + (1 if d == 'R' else -1)
         time_b += 1
