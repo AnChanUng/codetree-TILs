166851314f8b122f6daaf83d68ac295b3b3a0a0a
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-17 22:59:19 +0900

    Title: 만나는 그 순간, Time: 196ms, Memory: 40MB, Status: Passed - CodeTree

--- 240917/만나는 그 순간/the-moment-we-meet.py
+++ 240917/만나는 그 순간/the-moment-we-meet.py
@@ -1,24 +1,37 @@
 n, m = map(int, input().split())
-pos_a, pos_b = [0] * (1000000 + 1), [0] * (1000000 + 1)
+
+pos_a = [0] * (1000000 + 1)
+pos_b = [0] * (1000000 + 1)
 
 time_a = 1
 for _ in range(n):
     d, t = input().split()
-    for _ in range(int(t)):
-        pos_a[time_a] = pos_a[time_a - 1] + (1 if d == 'R' else -1)
+    t = int(t)
+    
+    for _ in range(t):
+        if d == 'R':
+            pos_a[time_a] = pos_a[time_a - 1] + 1
+        else:  
+            pos_a[time_a] = pos_a[time_a - 1] - 1
         time_a += 1
 
 time_b = 1
 for _ in range(m):
     d, t = input().split()
-    for _ in range(int(t)):
-        pos_b[time_b] = pos_b[time_b - 1] + (1 if d == 'R' else -1)
+    t = int(t)
+    
+    for _ in range(t):
+        if d == 'R':
+            pos_b[time_b] = pos_b[time_b - 1] + 1
+        else:  
+            pos_b[time_b] = pos_b[time_b - 1] - 1
         time_b += 1
 
 ans = -1
+
 for i in range(1, time_a):
     if pos_a[i] == pos_b[i]:
         ans = i
         break
-        
+
 print(ans)
