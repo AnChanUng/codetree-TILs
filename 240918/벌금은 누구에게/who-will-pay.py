8d84d605d86e1f54ee3c8967548e0dc9f4196304
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-18 21:30:10 +0900

    Title: 벌금은 누구에게, Time: 141ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240918/벌금은 누구에게/who-will-pay.py
+++ 240918/벌금은 누구에게/who-will-pay.py
@@ -5,17 +5,10 @@ array = []
 for _ in range(m):
     array.append(int(input()))
 
-cnt = 0
-for i in array:
-
-    if arr[i] != k:
-        arr[i] += 1
-        print(arr.index(arr[i]))
+for i in range(len(array)):
+    
+    arr[array[i]] += 1
+    
+    if arr[array[i]] == k:
+        print(arr.index(arr[arr[i]]))
         break
-    else:
-        arr[i] += 1
-        if arr[i] == k:
-            print(arr.index(arr[i]))
-            break
-        else:
-            print(-1)
