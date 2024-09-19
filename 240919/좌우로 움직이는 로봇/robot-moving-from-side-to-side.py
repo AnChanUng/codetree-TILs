f7165f30a183bc2745fd061490b56410cf69db7e
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-19 14:22:48 +0900

    Title: 좌우로 움직이는 로봇, Time: 225ms, Memory: 88MB, Status: Passed - CodeTree

--- 240919/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240919/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -2,37 +2,40 @@ n, m = map(int, input().split())
 a = [0] * (1000001)
 b = [0] * (1000001)
 
-res1 = 0
-time_a = 0
+time_a = 1
 for _ in range(n):
     t, d = input().split()
     t = int(t)
 
     for _ in range(1, t+1):
         if d == 'L':
-            res1 -= 1
-
+            a[time_a] = a[time_a-1] - 1
         elif d == 'R':
-            res1 += 1
-        a[time_a] = res1
+            a[time_a] = a[time_a-1] + 1
         time_a += 1
 
-res2 = 0
-time_b = 0
+time_b = 1
 for _ in range(m):
     t, d = input().split()
     t = int(t)
 
     for _ in range(1, t+1):
         if d == 'L':
-            res2 -= 1
+            b[time_b] = b[time_b-1] - 1
         elif d == 'R':
-            res2 += 1
-        b[time_b] = res2
+            b[time_b] = b[time_b-1] + 1
         time_b += 1
 
+if time_a < time_b:
+    for i in range(time_a, time_b):
+        a[i] = a[i-1]
+elif time_a > time_b:
+    for i in range(time_b, time_a):
+        b[i] = b[i-1]
+
 cnt = 0
-for i in range(1, time_a+1):
+time_max = max(time_a, time_b)
+for i in range(1, time_max):
     if a[i] == b[i] and a[i-1] != b[i-1]:
         cnt += 1
 
