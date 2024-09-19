cce28bd56fd82637e530f90ab1c00e88d390b93d
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-19 16:05:26 +0900

    Title: 선두를 지켜라 3, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240919/선두를 지켜라 3/keep-the-lead-3.py
@@ -0,0 +1,42 @@
+n, m = map(int, input().split())
+arr1 = [0] * 30
+arr2 = [0] * 30
+
+time_a = 1
+for _ in range(n):
+    v, t = map(int, input().split())
+    
+    for _ in range(t):
+        arr1[time_a] = arr1[time_a-1] + v
+        time_a += 1
+
+time_b = 1
+for _ in range(m):
+    v, t = map(int, input().split())
+    for _ in range(t):
+        arr2[time_b] = arr2[time_b-1] + v
+        time_b += 1
+
+leader = 0
+cnt = 0
+for i in range(1, time_a):
+    if arr1[i] < arr2[i]:
+        if leader == 2:
+            cnt += 1
+        leader = 1
+
+    elif arr1[i] > arr2[i]:
+        if leader == 1:
+            cnt += 1
+        leader = 2
+    
+    else:
+        if leader == 1:
+            cnt += 1
+        leader = 2
+        
+        if leader == 2:
+            cnt += 1
+        leader = 1
+
+print(cnt)
