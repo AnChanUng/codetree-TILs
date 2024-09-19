c8a84e638e73323d40e99c7e0a7e1888e603ddad
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-19 14:09:34 +0900

    Title: 좌우로 움직이는 로봇, Time: 149ms, Memory: 40MB, Status: Wrong Answer - CodeTree

--- 240919/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240919/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -2,37 +2,36 @@ n, m = map(int, input().split())
 a = [0] * (1000001)
 b = [0] * (1000001)
 
-i = 1
+res1 = 0
+time = 0
 for _ in range(n):
     t, d = input().split()
     t = int(t)
-    if d == 'R':
-        a[i] = a[i-1] + t
-    elif d == 'L':
-        a[i] = a[i-1] - t
-    i += 1
 
-j = 1
+    for _ in range(1, t+1):
+        if d == 'L':
+            res1 -= 1
+
+        elif d == 'R':
+            res1 += 1
+        a[time] = res1
+        time += 1
+
+res2 = 0
 for _ in range(m):
     t, d = input().split()
     t = int(t)
-    if d == 'R':
-        b[j] = b[j-1] + t
-    elif d == 'L':
-        b[j] = b[j-1] - t
-    j += 1
 
-leader = 0
-ans = 1
-for num in range(1, i):
-    if a[num] >= b[num]:
-        if leader == 2:
-            ans += 1
-        leader = 1
-    
-    elif a[num] <= b[num]:
-        if leader == 1:
-            ans += 1
-        leader = 2
+    for _ in range(1, t+1):
+        if d == 'L':
+            res2 -= 1
+        elif d == 'R':
+            res2 += 1
+        b[time] = res2
+        time += 1
+cnt = 0
+for i in range(1, time):
+    if a[i] == b[i] and a[i-1] != b[i-1]:
+        cnt += 1
 
-print(ans)
+print(cnt)
