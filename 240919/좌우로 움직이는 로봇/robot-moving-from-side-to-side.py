6940a05bf6e9f4ac5b6f01b77dc142761058084e
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-19 14:13:36 +0900

    Title: 좌우로 움직이는 로봇, Time: 155ms, Memory: 40MB, Status: Wrong Answer - CodeTree

--- 240919/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240919/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -3,7 +3,7 @@ a = [0] * (1000001)
 b = [0] * (1000001)
 
 res1 = 0
-time = 0
+time_a = 0
 for _ in range(n):
     t, d = input().split()
     t = int(t)
@@ -14,10 +14,11 @@ for _ in range(n):
 
         elif d == 'R':
             res1 += 1
-        a[time] = res1
-        time += 1
+        a[time_a] = res1
+        time_a += 1
 
 res2 = 0
+time_b = 0
 for _ in range(m):
     t, d = input().split()
     t = int(t)
@@ -27,10 +28,11 @@ for _ in range(m):
             res2 -= 1
         elif d == 'R':
             res2 += 1
-        b[time] = res2
-        time += 1
+        b[time_b] = res2
+        time_b += 1
+
 cnt = 0
-for i in range(1, time):
+for i in range(1, time_a+1):
     if a[i] == b[i] and a[i-1] != b[i-1]:
         cnt += 1
 
