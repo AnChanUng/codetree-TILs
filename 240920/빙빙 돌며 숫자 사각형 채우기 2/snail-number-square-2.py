ad93abe0dde6c06979f61e7fa5ee35c5ed303920
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-20 14:10:39 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 130ms, Memory: 25MB, Status: Passed - CodeTree

--- 240920/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
+++ 240920/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -4,23 +4,25 @@ array = [[0] * m for _ in range(n)]
 
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
-
-x = 0
-y = 0
-array[x][y] = 1
 dir_num = 1
+x, y = 0, 0
+array[x][y] = 1
 for i in range(2, n*m+1):
-    while True:
-        nx = x + dx[dir_num]
-        ny = y + dy[dir_num]    
-    
-        if nx < 0 or ny < 0 or nx >= n or ny >= m or array[nx][ny] != 0:
-            dir_num = (dir_num + 3) % 4
-        else:
-            array[nx][ny] = i
-            break
+    nx = x + dx[dir_num]
+    ny = y + dy[dir_num]
+
+    if nx >= 0 and nx < n and ny >= 0 and ny < m and array[nx][ny] == 0:
+        x = x + dx[dir_num]
+        y = y + dy[dir_num]
+        array[x][y] = i
+    else:
+        dir_num = (dir_num + 3) % 4
+        x = x + dx[dir_num]
+        y = y + dy[dir_num]
+        array[x][y] = i
+
 
 for i in range(n):
     for j in range(m):
-        print(array[i][j], end=" ")
+        print(array[i][j], end= " ")
     print()
