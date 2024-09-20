bc393d7403e15dee96588878748ab5baad41577d
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-20 16:42:55 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 121ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240920/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -0,0 +1,30 @@
+n = int(input())
+
+array = [[0] * n for _ in range(n)]
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+dir_num = 0
+x = n // 2
+y = n // 2
+array[x][y] = 1
+for i in range(2, n*n+1):
+    nx = x + dx[dir_num]
+    ny = y + dy[dir_num]
+
+    if 0 <= nx < n and 0 <= ny < n and array[nx][ny] == 0:
+        x = x + dx[dir_num]
+        y = y + dy[dir_num]
+        array[x][y] = i
+
+    else:
+        dir_num = (dir_num+3) % 4
+        x = x + dx[dir_num]
+        y = y + dy[dir_num]
+        array[x][y] = i
+
+for i in range(n):
+    for j in range(n):
+        print(array[i][j], end = " ")
+    print()
