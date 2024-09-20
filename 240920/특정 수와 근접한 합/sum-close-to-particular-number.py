33abb20ee108389c78670a4791bc1c637392dbbc
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-20 20:53:43 +0900

    Title: 특정 수와 근접한 합, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240920/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -0,0 +1,14 @@
+from itertools import combinations
+
+n, s = map(int, input().split())
+
+numbers = list(map(int, input().split()))
+
+numbers.sort()
+
+min_num = 10001
+for nums in combinations(numbers, len(numbers)-2):
+    if sum(nums) - s < min_num:
+        min_num = min(min_num, sum(nums))
+
+print(min_num - s)
