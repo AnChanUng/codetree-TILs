a71739978bcf2fbec872fe9492cf5d49ec49f328
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-22 21:25:51 +0900

    Title: 두 정수에 대한 연산값 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240922/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
+++ 240922/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -4,8 +4,8 @@ def solution(a, b):
     if a < b:
         small = a + 10
         big = b * 2
-        return small, big
+        return (small, big)
     elif a > b:
         small = b + 10
         big = a * 2
-        return small, big
+        return (small, big)
