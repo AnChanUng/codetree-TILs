ca9c189d5a58018fa9890fdc3c272d3dd8b636e9
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-22 21:28:26 +0900

    Title: 두 정수에 대한 연산값 2, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240922/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
+++ 240922/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -1,8 +1,8 @@
-a, b = map(int, input().split())
-
 def solution(a, b):
     small = min(a, b) + 10
     big = max(a, b) * 2
     return small, big
+
+a, b = map(int, input().split())
 
 print(*solution(a, b))
