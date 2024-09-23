de5ac7bbded59e026278412c83cadb0e55ca28f6
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-23 11:17:04 +0900

    Title: 일렬로 서있는 소 2, Time: 175ms, Memory: 25MB, Status: Passed - CodeTree

--- 240923/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240923/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -6,6 +6,5 @@ for i in range(len(numbers)):
     for j in range(i+1, len(numbers)):
         for k in range(j+1, len(numbers)):
             if numbers[i] <= numbers[j] <= numbers[k]:
-                print(numbers[i], numbers[j], numbers[k])
                 cnt += 1
 print(cnt)
