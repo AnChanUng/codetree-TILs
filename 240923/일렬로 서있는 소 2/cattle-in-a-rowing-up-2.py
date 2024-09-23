67e42400ce01ef4200e2241ee29304808d3156af
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-23 11:16:39 +0900

    Title: 일렬로 서있는 소 2, Time: 207ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240923/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240923/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -2,10 +2,10 @@ n = int(input())
 numbers = list(map(int, input().split()))
 
 cnt = 0 
-for i in range(n):
-    for j in range(i+1, n):
-        for k in range(j+1, n):
-            print(i, j, k)
+for i in range(len(numbers)):
+    for j in range(i+1, len(numbers)):
+        for k in range(j+1, len(numbers)):
             if numbers[i] <= numbers[j] <= numbers[k]:
+                print(numbers[i], numbers[j], numbers[k])
                 cnt += 1
 print(cnt)
