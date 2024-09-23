9dcc797291d27bde23530629a34e999e6fd6e9c7
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-23 11:35:22 +0900

    Title: 최고의 13위치, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

--- 240923/최고의 13위치/best-place-of-13.py
+++ 240923/최고의 13위치/best-place-of-13.py
@@ -2,12 +2,12 @@ n = int(input())
 
 array = []
 for i in range(n):
-    array.append(list(input().split()))
+    array.append(list(map(int, input().split())))
 
 max_cnt = 0
 for i in range(n):
     for j in range(n-2):
-        if max_cnt < array[i][j:j+3].count('1'):
-            max_cnt = array[i][i:j+3].count('1')
+        if max_cnt < sum(array[i][j:j+3]):
+            max_cnt = sum(array[i][j:j+3])
  
 print(max_cnt)
