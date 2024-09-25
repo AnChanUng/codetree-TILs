ff1898aa122296b2e9e0871146304f1c4be80f75
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-25 10:23:23 +0900

    Title: 밭의 높이를 고르게하기, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240925/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 240925/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -8,7 +8,6 @@ for i in range(n-t+1):
         res += abs(h - array[j])
         if res < min_res:
             min_res = min(min_res, res)
-            res = min_res
     #print("res", res)
 
 print(min_res)
