bc21a2255a35b5a8274276abe17cc60e9b164f1a
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-25 09:40:17 +0900

    Title: 아름다운 수열 2, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240925/아름다운 수열 2/beautiful-sequence-2.py
@@ -0,0 +1,14 @@
+from itertools import permutations
+
+n, m = map(int, input().split())
+numbers = list(map(int, input().split()))
+check = list(map(int, input().split()))
+
+cnt = 0
+for i in range(n-m+1):
+    for num in permutations(check, m):
+        num = list(num)
+        if check[i:i+m] == num:
+            cnt += 1
+    
+print(cnt)
