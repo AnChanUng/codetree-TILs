fe585d610e27626546b596e849a3c9fede69e793
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-25 15:38:33 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 183ms, Memory: 26MB, Status: Passed - CodeTree

--- 240925/한 가지로 열리는 자물쇠/one-way-lock.py
+++ 240925/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -1,15 +1,11 @@
-from itertools import permutations
-
 n = int(input())
-numbers = list(map(int, input().split()))
+a, b, c = map(int, input().split())
 
 cnt = 0
-def minus(a, b, c):
-    global cnt
-    if abs(x-y) <= 2 and abs(b-c) <= 2 and abs(c-a) <= 2:
-        cnt += 1
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        for k in range(1, n+1):       
+            if abs(a-i) <= 2 or abs(b-j) <= 2 or abs(c-k) <= 2:
+                cnt += 1
 
-for x, y, z in permutations(numbers, 3):
-    minus(x, y, z)
-
-print(n*n*n - cnt)
+print(cnt)
