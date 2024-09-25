6852d8ca3ffe5da726da08f2cbf69f0d09d83653
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-25 10:56:22 +0900

    Title: 밭의 높이를 고르게하기, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240925/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 240925/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -3,12 +3,11 @@ array = list(map(int, input().split()))
 
 min_res = 10001
 for i in range(n-t+1):
-    res = 0
+    total = 0
     for j in range(t):
-        res += abs(h - array[i+j])
-        print("abs(h - array[i+j])", abs(h - array[i+j]))
-        if res < min_res:
-            min_res = min(min_res, res)
-    print("res", res)
+        res = abs(h - array[i+j])
+        total += res
+    
+    min_res = min(min_res, total)
 
 print(min_res)
