8c21fb469c05d5c89cdafce1be602406a512f487
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-27 10:54:31 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 180ms, Memory: 26MB, Status: Passed - CodeTree

--- 240927/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 240927/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -1,26 +1,17 @@
 n = int(input())
-a1, b1, c1 = list(map(int, input().split()))
-a2, b2, c2 = map(int, input().split())
+a, b, c = tuple(map(int, input().split()))
+a2, b2, c2 = tuple(map(int, input().split()))
 
-array1 = set()
-for i in range(a1-2, a1+2+1):
-    for j in range(b1-2, b1+2+1):
-        for k in range(c1-2, c1+2+1):
-            
-            ni = (i - 1) % n + 1
-            nj = (j - 1) % n + 1
-            nk = (k - 1) % n + 1
-            array1.add((ni, nj, nk))
+cnt = 0
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        for k in range(1, n + 1):
+            if (abs(a - i) <= 2 or abs(a - i) >= n - 2) and (abs(b - j) <= 2 or abs(b - j) >= n - 2) and \
+               (abs(c - k) <= 2 or abs(c - k) >= n - 2):
+                cnt += 1
+			
+            elif (abs(a2 - i) <= 2 or abs(a2 - i) >= n - 2) and (abs(b2 - j) <= 2 or abs(b2 - j) >= n - 2) and \
+               (abs(c2 - k) <= 2 or abs(c2 - k) >= n - 2):
+                cnt += 1
 
-array2 = set()
-for i in range(a2-2, a2+2+1):
-    for j in range(b2-2, b2+2+1):
-        for k in range(c2-2, c2+2+1):
-            ni = (i-1) % n + 1
-            nj = (j-1) % n + 1
-            nk = (k-1) % n + 1
-            
-            array2.add((ni, nj, nk))
-
-all_arr = array1 | array2
-print(len(all_arr))
+print(cnt)
