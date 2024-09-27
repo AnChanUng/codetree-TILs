04257a56a689c2b5ab218e46fd633a1bad8762c9
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-27 09:43:53 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240927/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 240927/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -18,13 +18,18 @@ array2 = []
 for i in range(a2-2, a2+2+1):
     for j in range(b2-2, b2+2+1):
         for k in range(c2-2, c2+2+1):
-            if i < 1:
-                i = n+i 
-            elif j < 1: 
-                j = n+j
-            elif k < 1:
-                k = n+k
-            array2.append((i, j, k))
+            
+            if 1 <= i <= n and 1 <= j <= n and 1 <= k <= n:
+            
+                if i < 1:
+                    i = n+i 
+                elif j < 1: 
+                    j = n+j
+                elif k < 1:
+                    k = n+k
+                array2.append((i, j, k))
+            
+            continue
 
 cnt = 0
 for i in array1:
