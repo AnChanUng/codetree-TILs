87691c510acc9a518c3c7eae7e4e65ad0a1f4766
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-30 20:07:06 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240930/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240930/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -2,9 +2,9 @@ def dfs(x, y):
     dxs = [0, 1]
     dys = [1, 0]
 
-    for i in range(2):
-        new_x = x + dxs[i]
-        new_y = y + dys[i]
+    for dx, dy in zip(dxs, dys):
+        new_x = x + dx
+        new_y = y + dy
 
         if 0 <= new_x < n and 0 <= new_y < m:
 
