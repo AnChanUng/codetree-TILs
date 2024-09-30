c8616b91a6e552b75f0a0984d465adf025e2f570
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-30 19:49:05 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240930/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240930/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -2,17 +2,17 @@ n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
 visited = [[0] * m for _ in range(n)]
 
-def in_range(x, y):
-    return 0 <= x < n and 0 <= y < m
+# def in_range(x, y):
+#     return 0 <= x < n and 0 <= y < m
 
-def can_go(x, y):
-    if not in_range(x, y):
-        return False
+# def can_go(x, y):
+#     if not in_range(x, y):
+#         return False
     
-    if visited[x][y] or graph[x][y] == 0:
-        return False
+#     if visited[x][y] or graph[x][y] == 0:
+#         return False
     
-    return True
+#     return True
 
 def dfs(x, y):
     dxs = [0, 1]
@@ -22,9 +22,11 @@ def dfs(x, y):
         new_x = x + dx
         new_y = y + dy
 
-        if can_go(new_x, new_y):
-            visited[new_x][new_y] = 1
-            dfs(new_x, new_y)
+        if 0 <= new_x < n and 0 <= new_y < m:
+
+            if visited(new_x, new_y) == 0 and graph[new_x][new_y] == 1: 
+                visited[new_x][new_y] = 1
+                dfs(new_x, new_y)
 
 visited[0][0] = 1
 dfs(0, 0)
