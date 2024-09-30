d8c49d00ff7e7f65d364655250ad20dc668cf8e5
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-09-30 11:14:44 +0900

    Title: 최고의 33위치, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

--- 240930/최고의 33위치/best-place-of-33.py
+++ 240930/최고의 33위치/best-place-of-33.py
@@ -4,8 +4,8 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 cnt = 0
 def solution(x, y):
     global cnt
-    for i in range(0, 3):
-        for j in range(0, 3):
+    for i in range(x, 3+x):
+        for j in range(y, 3+y):
             if arr[i][j] == 1:
                 cnt += 1
     
@@ -14,7 +14,7 @@ def solution(x, y):
 max_cnt = 0
 for i in range(0, n-2):
     for j in range(0, n-2):
-     #   print(solution(i, j), end = " ")
+     #  print(solution(i, j), end = " ")
         max_cnt = max(max_cnt, solution(i, j))
         cnt = 0
     #print()
