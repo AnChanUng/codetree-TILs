f75f03a25e20418892fdfd5a614893caf03297d8
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-10-01 20:49:54 +0900

    Title: 마을 구분하기, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 241001/마을 구분하기/seperate-village.py
+++ 241001/마을 구분하기/seperate-village.py
@@ -1,19 +1,35 @@
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
+visited = [[0 for _ in range(n)] for _ in range(n)]
+section = []
 
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+answer = []
 cnt = 0
 def dfs(x, y):
+    visited[x][y] = 1
     global cnt
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+    cnt += 1
 
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
 
-        if graph[nx][ny] == 1:
-            cnt += 1
+        if 0 <= nx < n and 0 <= ny < n:
+            if not visited[nx][ny] and graph[nx][ny] == 1:
+                dfs(nx, ny)
 
-dfs(0, 0)
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j] and graph[i][j] == 1:
+            cnt = 0
+            dfs(i, j)
+            answer.append(cnt)
+        
+answer.sort()
+print(len(answer))
 
-print(array)
+for i in answer:
+    print(i)
