f3862af0cb4ec98badfc010a4fda6a8fb0595d63
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-10-04 19:57:43 +0900

    Title: 제거한 숫자의 최솟값, Time: 163ms, Memory: 29MB, Status: Wrong Answer - CodeTree

--- 241004/제거한 숫자의 최솟값/minimum-of-numbers-removed.py
+++ 241004/제거한 숫자의 최솟값/minimum-of-numbers-removed.py
@@ -9,12 +9,9 @@ n = int(input())
 array = deque(sorted(map(int, input().split())))
 
 max_num = 0
-while True:
+while len(array) > 1:
     front = array.popleft()
     end = array.pop()
     max_num = max(front+end, max_num)
-
-    if len(array) <= 0:
-        break
 
 print(max_num)
