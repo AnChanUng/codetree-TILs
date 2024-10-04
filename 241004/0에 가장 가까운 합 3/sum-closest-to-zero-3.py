976ecd0e84108f783d6a3ae00810392f8432e80e
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-10-04 20:41:12 +0900

    Title: 0에 가장 가까운 합 3, Time: 132ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241004/0에 가장 가까운 합 3/sum-closest-to-zero-3.py
+++ 241004/0에 가장 가까운 합 3/sum-closest-to-zero-3.py
@@ -1,11 +1,14 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 numbers = list(map(int, input().split()))
 
 min_num = 1000001
 for i in range(len(numbers)):
     for j in range(i+1, len(numbers)):
-        num = abs(numbers[i] + numbers[j])
-        if num < min_num:
+        num = numbers[i] + numbers[j]
+        if abs(num) < min_num:
             min_num = min(min_num, num)
 
-print(max_num)
+print(min_num)
