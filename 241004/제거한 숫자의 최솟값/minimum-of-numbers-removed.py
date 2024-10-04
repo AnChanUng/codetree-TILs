fe4967228062f331ca6c025d04ba6b40fdb74890
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-10-04 20:08:54 +0900

    Title: 제거한 숫자의 최솟값, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241004/제거한 숫자의 최솟값/minimum-of-numbers-removed.py
+++ 241004/제거한 숫자의 최솟값/minimum-of-numbers-removed.py
@@ -1,16 +1,20 @@
 import sys
+from collections import deque
+
 input = sys.stdin.readline
 
 n = int(input().strip())
 array = sorted(map(int, input().split()))
 
-min_m = 0
-for i in range((n+1)//2):
-    if i < n//2:
-        pair_sum = array[i] + array[n-1-i]
+min_m = 
+
+
+for i in range((n + 1) // 2):
+    if i < n // 2:
+        pair_sum = array[i] + array[n - 1 - i]
         min_m = max(min_m, pair_sum)
 
 if n % 2 == 1:
-    min_m = max(min_m, array[n//2])
+    min_m = max(min_m, array[n // 2])
 
 print(min_m)
