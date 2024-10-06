0d4599181bbddccbdcc4b30b58524f17dd32178f
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-10-06 17:56:00 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 153ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241006/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
+++ 241006/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -1,31 +1,28 @@
 from collections import deque
-
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-
 def bfs(x, y):
     queue = deque()
     queue.append((x, y))
-    visited[x][y] = True
-    
+    visited[x][y] = 1
+
     while queue:
         x, y = queue.popleft()
 
-        if x == n - 1 and y == m - 1:
+        if x == n - 1 or y == m - 1:
             return 1
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and graph[nx][ny] == 1:
-                visited[nx][ny] = True
+            if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == 0 and graph[nx][ny] == 1:
+                visited[nx][ny] = 1
                 queue.append((nx, ny))
-    
     return 0
 
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-visited = [[False] * m for _ in range(n)]
+visited = [[0] * m for _ in range(n)]
 
 print(bfs(0, 0))
