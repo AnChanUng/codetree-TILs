12e7c7fa72b045f30ec5503442a42da00bcdd914
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-10-07 11:35:35 +0900

    Title: 갈 수 있는 곳들, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241007/갈 수 있는 곳들/places-can-go.py
+++ 241007/갈 수 있는 곳들/places-can-go.py
@@ -3,13 +3,18 @@ from collections import deque
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-cnt = 1
+cnt = 0
 def bfs(x, y):
     global cnt
+    
+    if graph[x][y] == 1:
+        return 0
+    
     queue = deque()
     queue.append((x, y))
+    #print("queue", queue)
     visited[x][y] = 0
-
+    cnt += 1
     while queue:
         x, y = queue.popleft()
         #print("x, y", x, y)
@@ -17,19 +22,18 @@ def bfs(x, y):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == 1 and graph[nx][ny] == 0:
+            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 0 and visited[nx][ny] == 1:
                 visited[nx][ny] = 0
                 #print(visited)
                 queue.append((nx, ny))
                 cnt += 1
                 #print("cnt", cnt)
-    
     return 1
 
 n, k = map(int, input().split())
 
 graph = [list(map(int, input().split())) for _ in range(n)]
-visited = [[1] * n for _ in range(n)]
+visited = [[1] * n for _ in range(n+1)]
 
 for _ in range(k):
     r, c = map(int, input().split())
