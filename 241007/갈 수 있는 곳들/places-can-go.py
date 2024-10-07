e6abbe588a47f51118cc927f56cf3d5ffb51e9af
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-10-07 11:21:39 +0900

    Title: 갈 수 있는 곳들, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241007/갈 수 있는 곳들/places-can-go.py
+++ 241007/갈 수 있는 곳들/places-can-go.py
@@ -3,7 +3,7 @@ from collections import deque
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-cnt = 0
+cnt = 1
 def bfs(x, y):
     global cnt
     queue = deque()
@@ -12,15 +12,17 @@ def bfs(x, y):
 
     while queue:
         x, y = queue.popleft()
-        
+        #print("x, y", x, y)
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
             if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == 1 and graph[nx][ny] == 0:
                 visited[nx][ny] = 0
+                #print(visited)
                 queue.append((nx, ny))
                 cnt += 1
+                #print("cnt", cnt)
     
     return 1
 
