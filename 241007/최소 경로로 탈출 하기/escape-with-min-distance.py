6f5ac42b7b0881fa66f08486aed88493f9f67ad8
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-10-07 17:44:22 +0900

    Title: 최소 경로로 탈출 하기, Time: 135ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241007/최소 경로로 탈출 하기/escape-with-min-distance.py
+++ 241007/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -4,23 +4,23 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 cnt = 0
 
-def bfs(x, y):    
-    global cnt 
+def bfs(x, y):
+    global cnt
     queue = deque()
     queue.append((x, y))
     visited[x][y] = 1
-    cnt += 1
-    
+    cnt = 1  
+
     while queue:
         x, y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
+
             if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == 0 and graph[nx][ny] == 1:
-                visited[nx][ny] = cnt
-                queue.append((x, y))
-                cnt += 1
-    
+                visited[nx][ny] = 1  
+                queue.append((nx, ny))  
+                cnt += 1  
     return cnt
 
 n, m = map(int, input().split())
