50fca49693e978c570695cb3b39138f95cdc019f
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-10-08 19:25:53 +0900

    Title: 비를 피하기, Time: 149ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241008/비를 피하기/stay-out-of-rain.py
+++ 241008/비를 피하기/stay-out-of-rain.py
@@ -2,7 +2,6 @@ from collections import deque
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-
 def bfs(x, y):
     queue = deque()
     queue.append((x, y))
@@ -23,6 +22,7 @@ def bfs(x, y):
                 visited[nx][ny] = 1
                 distance[nx][ny] = distance[x][y] + 1
                 queue.append((nx, ny))
+    return -1
 
 n, h, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
@@ -34,5 +34,7 @@ for i in range(n):
         if graph[i][j] == 2:
             result[i][j] = bfs(i, j)
 
-for row in result:
-    print(" ".join(map(str, row)))
+for i in range(n):
+    for j in range(n):
+        print(result[i][j], end = " ")
+    print()
