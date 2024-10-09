a64a2d082fb96316862303370c969ed5b62b4fa5
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-10-09 21:40:57 +0900

    Title: 상한 귤, Time: 386ms, Memory: 30MB, Status: Wrong Answer - CodeTree

--- 241009/상한 귤/oranges-have-gone-bad.py
+++ 241009/상한 귤/oranges-have-gone-bad.py
@@ -2,38 +2,40 @@ from collections import deque
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-
 def bfs(x, y):
     queue = deque()
     queue.append((x, y))
-    #print("queue", queue)
     visited = [[0] * n for _ in range(n)]
-    visited[x][y] = 1    
+    visited[x][y] = 1
+    distance = [[0] * n for _ in range(n)]
     
     while queue:
         x, y = queue.popleft()
-        #print("x, y", x, y)
+        
+        if graph[x][y] == 2:
+            return distance[x][y]
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == 0 and graph[nx][ny] == 1:
-                visited[nx][ny] = visited[x][y] + 1
-                queue.append((x, y))
-
-    return 0
+            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] and graph[nx][ny] != 0:
+                visited[nx][ny] = 1
+                distance[nx][ny] = distance[x][y] + 1
+                queue.append((nx, ny))
+    return -1
 
 n, k = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-
 result = [[-1] * n for _ in range(n)]
 for i in range(n):
     for j in range(n):
         if graph[i][j] == 1:
-            bfs(i, j)
+            result[i][j] = bfs(i, j)
         elif graph[i][j] == 2:
             result[i][j] = 0
-        elif graph[i][j] == 0:
-            result[i][j] = -1
 
-print(result)
+for i in range(n):
+    for j in range(n):
+        print(result[i][j], end = " ")
+    print()
