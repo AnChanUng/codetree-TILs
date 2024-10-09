e693722ced104a41b1086adff02a65f2e011dc41
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-10-09 16:06:33 +0900

    Title: k개의 벽 없애기, Time: 160ms, Memory: 32MB, Status: Wrong Answer - CodeTree

--- 241009/k개의 벽 없애기/remove-k-walls.py
+++ 241009/k개의 벽 없애기/remove-k-walls.py
@@ -1,36 +1,42 @@
 from collections import deque
 
+# 방향 설정
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 def bfs(x, y):
+    global k
     queue = deque()
     queue.append((x, y))    
     visited[x][y] = 0
 
     while queue:
         x, y = queue.popleft()
-
-        if graph[r2-1][c2-1] == 0:
-            return visited[r2-1][c2-1]
-
-        if k <= -1
-            return -1
+        
+        if (x, y) == (r2 - 1, c2 - 1):
+            return visited[x][y]
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] and (graph[nx][ny] == 0 or graph[nx][ny] == 1):
-                graph[nx][ny] = 1
-                visited[nx][ny] = visited[nx][ny] + 1
-                queue.append((nx, ny))
+            if 0 <= nx < n and 0 <= ny < n:
+                if visited[nx][ny] == 0 or visited[nx][ny] > visited[x][y] + 1:
+                    if graph[nx][ny] == 0:
+                        visited[nx][ny] = visited[x][y] + 1
+                        queue.append((nx, ny))
+                    elif graph[nx][ny] == 1 and k > 0:
+                        visited[nx][ny] = visited[x][y] + 1
+                        queue.append((nx, ny))
+                        k -= 1                
     return -1
 
+# 입력 처리
 n, k = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
 visited = [[0] * n for _ in range(n)]
 r1, c1 = map(int, input().split())
 r2, c2 = map(int, input().split())
 
-print(bfs(r1-1, c1-1))
+# BFS 실행 및 결과 출력
+print(bfs(r1 - 1, c1 - 1))
