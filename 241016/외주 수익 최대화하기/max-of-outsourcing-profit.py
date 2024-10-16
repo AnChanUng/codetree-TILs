b6ba3ef727f2ad3f539348590d2827689b046eda
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-10-16 11:32:29 +0900

    Title: 외주 수익 최대화하기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241016/외주 수익 최대화하기/max-of-outsourcing-profit.py
+++ 241016/외주 수익 최대화하기/max-of-outsourcing-profit.py
@@ -4,13 +4,16 @@ for i in range(n):
     n, p = map(int, input().split())
     graph.append([n, p])
 
-sum_t = 0
-max_t = 0
-max_n = 0
+max_sum_t = 0
 for i in range(len(graph)):
-    if max_n <= n:
-        max_n += graph[i][0]
-        sum_t += graph[i][1]
-    max_t = max(max_t, sum_t)
+    sum_n = 0
+    sum_t = 0
+    for j in range(i, len(graph)):
+        if sum_n <= n:  
+            sum_n += graph[j][0]
+            sum_t += graph[j][1]
+        print("sum_n", sum_n)
+        print("sum_t", sum_t)
+    max_sum_t = max(max_sum_t, sum_t)
 
-print(max_t)
+print(max_sum_t)
