69a71d3049b6e960233dcf59aaa13309f1a7e20b
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-11-08 21:15:51 +0900

    Title: 미는 횟수, Time: 47ms, Memory: 15MB, Status: Passed - CodeTree

--- 241108/미는 횟수/number-of-pushes.py
+++ 241108/미는 횟수/number-of-pushes.py
@@ -1,17 +1,21 @@
 a = input()
 b = input()
 
-re = False
 if len(a) != len(b):
     print(-1)
 else:
-    for i in range(len(a)):
-        res = a[i:] + a[:i]
+    n = 0
+    flag = -1
 
-        if res == b:
-            print(i)
-            re = True
+    for i in range(len(a)):
+        a = a[-1] + a[:-1]
+        n += 1
+
+        if a == b:
+            flag = 1
             break
 
-    if re == False:
+    if flag == 1:
+        print(n)
+    else:
         print(-1)
