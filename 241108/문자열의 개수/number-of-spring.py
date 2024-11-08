63bbdbc2fd35bc473d93e7b5008f9c2860f12814
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-11-08 21:01:27 +0900

    Title: 문자열의 개수, Time: 51ms, Memory: 15MB, Status: Passed - CodeTree

--- 241108/문자열의 개수/number-of-spring.py
+++ 241108/문자열의 개수/number-of-spring.py
@@ -1,15 +1,15 @@
 array = []
+cnt = 0
 while True:
     word = input()
     array.append(word)
     
-    if word == '0':
-        if len(array) % 2 == 0:
-            print(len(array)-1)
-        else:
-            print(len(array))
-            
-        for i in range(len(array)):
-            if i % 2 == 0:
-                print(array[i])
+    if array[cnt] == '0':
         break
+
+    cnt += 1
+
+print(cnt)
+
+for i in range(0, cnt, 2):
+    print(array[i])
