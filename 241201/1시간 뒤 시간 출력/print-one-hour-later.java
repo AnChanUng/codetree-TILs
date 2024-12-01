eab4114516437086c320bf14f28dac6a138dfab5
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-12-01 13:30:04 +0900

    Title: 1시간 뒤 시간 출력, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241201/1시간 뒤 시간 출력/print-one-hour-later.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String[] strArr = s.split(":");
+        int h = Integer.parseInt(strArr[0]) + 1;
+        int m = Integer.parseInt(strArr[1]);
+        System.out.println(h + ":" + m);
+    }
+}
