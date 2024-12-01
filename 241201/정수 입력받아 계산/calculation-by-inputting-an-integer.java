80b0e2e9ae6e8cdbff9eea701c9278e8e5656662
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-12-01 12:32:25 +0900

    Title: 정수 입력받아 계산, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241201/정수 입력받아 계산/calculation-by-inputting-an-integer.java
+++ 241201/정수 입력받아 계산/calculation-by-inputting-an-integer.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+        int a;
+        a = sc.nextInt();
 
         result = a * 2 + 3;
         System.out.print(result);
