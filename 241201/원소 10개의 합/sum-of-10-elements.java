675be9049ee19371527e6d5b68b2fdecec88f227
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-12-01 23:06:26 +0900

    Title: 원소 10개의 합, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 241201/원소 10개의 합/sum-of-10-elements.java
+++ 241201/원소 10개의 합/sum-of-10-elements.java
@@ -4,11 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        int num;
+        int[] arr = new int[10];
         int sum = 0;
+
         for (int i = 0; i < 10; i++) {
-            num = sc.nextInt();
-            sum += num;
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            sum += arr[i];
         }
         System.out.print(sum);
     }
