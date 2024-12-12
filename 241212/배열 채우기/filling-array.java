a3f2cb3516805e7a54c1540e838c348884dcca6d
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-12-12 11:14:01 +0900

    Title: 배열 채우기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241212/배열 채우기/filling-array.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int[] arr = new int[10];
+
+        for (int i=0; i<10; i++) {
+            int number = sc.nextInt();
+            if (number == 0) {
+                break;
+            } else {
+                arr[i] = number;
+            }
+        }
+
+        for (int i=9; i>=0; i--) {
+            if (arr[i] != 0) 
+                System.out.print(arr[i] + " ");
+        }
+    }
+}
