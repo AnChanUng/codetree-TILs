999fea5a53af98c3180fb1034000419d07ad542c
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-12-16 22:31:52 +0900

    Title: 100 도달하기, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

--- 241216/100 도달하기/reach-100.java
+++ 241216/100 도달하기/reach-100.java
@@ -6,19 +6,18 @@ public class Main {
 
         int n = sc.nextInt();
         int[] arr = new int[1001];
-        int cnt = 3;
+        int cnt = 1;
         arr[0] = 1;
         arr[1] = n;
-        for (int i=2; i<1001; i++) {
-            arr[i] = arr[i-1] + arr[i-2];
-
-            if (arr[i] > 100) {
-                break;
-            }
+        
+        while(true) {
             cnt++;
+            arr[cnt] = arr[cnt-1] + arr[cnt-2];
+            if(arr[cnt] > 100)
+                break;
         }
         
-        for (int i=0; i<cnt; i++) {
+        for (int i=0; i<=cnt; i++) {
             System.out.print(arr[i] + " ");
         }
     }
