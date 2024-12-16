d8510251a1e39a4ff822ffa95e2ecf428231d01e
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-12-16 22:27:24 +0900

    Title: 100 도달하기, Time: 125ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 241216/100 도달하기/reach-100.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[9];
+        arr[0] = 1;
+        arr[1] = n;
+        for (int i=2; i<1001; i++) {
+            arr[i] = arr[i-1] + arr[i-2];
+
+            if (arr[i] > 100) {
+                break;
+            } 
+        }
+        
+        for (int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
