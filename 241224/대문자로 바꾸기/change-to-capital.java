4c43dca7853581a0f98bc34f1e7080bf5218ea2f
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2024-12-24 19:09:43 +0900

    Title: 대문자로 바꾸기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241224/대문자로 바꾸기/change-to-capital.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[][] arr = new String[5][3];
+
+        for (int i=0; i<5; i++) {
+            for (int j=0; j<3; j++) {
+                String chr = sc.next();
+                arr[i][j] = chr.toUpperCase();
+            }
+        }
+         
+        for (int i=0; i<5; i++) {
+            for (int j=0; j<3; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
