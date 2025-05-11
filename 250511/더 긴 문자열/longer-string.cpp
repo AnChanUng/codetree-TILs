d9cfa793481cf6c5e0d059f9d78d8f5a9aa32498
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2025-05-11 16:13:25 +0900

    Title: 더 긴 문자열, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250511/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str1;
+    string str2;
+
+    cin >> str1;
+    cin >> str2;
+
+    int len1 = str1.length();
+    int len2 = str2.length();
+    
+    if(len1 > len2) {
+        cout << str1 << " " << len1;
+    } else if(len1 < len2) {
+        cout << str2 << " " << len2;
+    } else {
+        cout << "same";
+    }
+
+    return 0;
+}
